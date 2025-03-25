3df4270ae5899e065714a80ddd05df07c31c5c47
fredy-tan <fredy_tantri@yahoo.co.id>
2025-03-25 15:26:00 +0800

    Concurrent with Handler

--- app/src/main/java/com/example/week9classsession/ConcurrentDemoActivity.java
+++ app/src/main/java/com/example/week9classsession/ConcurrentDemoActivity.java
@@ -3,6 +3,8 @@ package com.example.week9classsession;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
@@ -18,33 +20,78 @@ public class ConcurrentDemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // ConcurrentDemo
+//        Log.d(TAG, "Main Thread prints this");
+//        ExecutorService executor = Executors.newFixedThreadPool(3);
+//        for (int i = 0; i < 4; i++) { // can you guess the output log if i<4?
+//            String k = String.valueOf(i); // used for printing below
+//            executor.execute(new Runnable() { // this task is executed by the background thread
+//                @Override
+//                public void run() {
+//                    Log.d(TAG, "This is executed by background thread no " + k);
+//                    try {
+//                        Thread.sleep(100);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    Log.d(TAG, "This is also executed by background thread no " + k);
+//                }
+//            });
+//        }
+//        // this is in main thread
+//        try {
+//            Thread.sleep(50);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        Log.d(TAG, "Main Thread prints this again");
 
-        Log.d(TAG, "Main Thread prints this");
+        // Sharing data between main thread and background thread
+//        int s = 0;
+//        final Container<Integer> cs = new Container<>(s);
+//        ExecutorService executor = Executors.newSingleThreadExecutor();
+//        executor.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                // a new thread
+//                int s1 = cs.get() + 1;
+//                cs.set(s1);
+//            }
+//        });
 
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // Using handler from the background thread to execute task in main thread
+        // main thread (i.e. UI thread)
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Looper uiLooper = Looper.getMainLooper(); // get the main looper
+        final Handler handler = new Handler(uiLooper); // get the handler for the main thread
 
-        for (int i = 0; i < 4; i++) { // can you guess the output log if i<4?
-            String k = String.valueOf(i); // used for printing below
-            executor.execute(new Runnable() { // this task is executed by the background thread
-                @Override
-                public void run() {
-                    Log.d(TAG, "This is executed by background thread no " + k);
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    Log.d(TAG, "This is also executed by background thread no " + k);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                // instructions performed in the child thread
+                Log.d(TAG, "run: Child thread executing something");
+
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-            });
-        }
 
-        // this is in main thread
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        Log.d(TAG, "Main Thread prints this again");
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //UI Thread will receive and run this
+                        Log.d(TAG, "run: Main thread execute this after " +
+                                "background thread finish the task");
+                    }
+                });
+            }
+        });
     }
+}
+
+class Container<T>{
+    T value;
+    Container(T v) { this.value = v; }
+    void set(T v) { this.value = v; }
+    T get() { return this.value; }
 }
